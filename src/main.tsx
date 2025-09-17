adf1fb4ae67686e0decfbac391c1c8dcca87a65a
Christina_5P <christina.ahman@outlook.com>
2025-09-17 08:13:24 +0000

    change main.tsx to cooming soon.Change to false when I am ready

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import ComingSoon from "./ComingSoon";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+const showComingSoon = true; // ⬅ byt till false när du är redo
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {showComingSoon ? <ComingSoon /> : <App />}
+  </React.StrictMode>
+);
