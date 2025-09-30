df946e84ce8753c1e70382fbc116dafdd9893e99
Christina_5P <christina.ahman@outlook.com>
2025-09-30 18:51:16 +0000

    redigera storlek på logo och favicon

--- src/components/HouseHandsLogo.jsx
+++ src/components/HouseHandsLogo.jsx
@@ -5,9 +5,10 @@ import LogoSymbol from '../assets/trygghandlogo.png';
 const HouseHandsLogo = () => (
   <div className="flex items-center justify-center">
     <img
+      style={{ height: "134px", width: "172px" }}
       src={LogoSymbol}
       alt="Trygg Hand logotyp"
-      className="h-16 w-auto rounded-lg shadow"
+      className="h-[20px] w-[20px] !h-[28px] !w-[28px] rounded-lg shadow"
       onError={e => { e.target.onerror = null; e.target.src = "https://placehold.co/100x100/ef4444/ffffff?text=Err"; }}
     />
   </div>
